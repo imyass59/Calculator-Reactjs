2dd502dd201b4cbe5fa55fa2f347c77d528650a2
Ilyass Elkhadiri <m.imyass.59@gmail.com>
2022-09-05 16:44:51 +0100

    Create interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import CustomButton from "./components/CustomButton/CustomButton";
+import { useState } from "react";
 
 function App() {
-  const digits = [1,2,3,4,5,6,7,8,9,0,".","="];
-  const symbols = ["/","*","+","-","DEL"]
+  const [calc,setCalc] = useState("");
+  const [result,setResult] = useState("");
+  const digits = [1,2,3,4,5,6,7,8,9,0];
+  const symbols = ["/","*","+","-"]
+
+  const updateCalcul = value => setCalc(calc + value) && setResult(eval(calc+value).toString());
   return (
-    <div className="App bg-slate-900">
+    <div className="App bg-slate-900 w-96">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="bg-slate-200 flex flex-col rounded-md">
-          <section id="screen" className="p-6 text-end text-3xl bg-slate-200">
-            <span className="text-lg">({(0)})</span>
-            0
+        <div className="bg-slate-200 flex flex-col rounded-md w-96">
+          <section id="screen" className="p-6 text-end text-3xl bg-slate-200 break-words">
+            <span className="text-lg">({(result || 0)})</span>
+            {calc || 0}
           </section>
           <section id="symbol" className="grid grid-cols-5">
             {
-              symbols.map((symbol) => <CustomButton key={symbol} params={symbol} Bgcolor="bg-indigo-700" ColorHover="bg-indigo-500" />)
+              symbols.map((symbol) => <button key={symbol}  onClick={() => updateCalcul(symbol.toString())}><CustomButton params={symbol} Bgcolor="bg-indigo-700" ColorHover="bg-indigo-500" /></button>)
+            }
+            {
+              <button onClick={() => setCalc("") && setResult("")} >
+                <CustomButton params="DEL" Bgcolor="bg-indigo-700" ColorHover="bg-indigo-500" />
+              </button>
             }
           </section>
           <section id="digit" className="grid grid-cols-3">
             {
-              digits.map((digit) => <CustomButton key={digit} params={digit} Bgcolor="bg-gray-800" ColorHover="bg-gray-600" />)
+              digits.map((digit) => <button key={digit} onClick={()=> updateCalcul(digit.toString())}><CustomButton params={digit} Bgcolor="bg-gray-800" ColorHover="bg-gray-600" /></button>)
+            }
+            {
+              (
+                <>
+                  <button onClick={()=> updateCalcul(".")}><CustomButton params="." Bgcolor="bg-gray-800" ColorHover="bg-gray-600" /></button>
+                  <button><CustomButton params="=" Bgcolor="bg-gray-800" ColorHover="bg-gray-600" /></button>
+                </>
+              )
             }
           </section>
         </div>
